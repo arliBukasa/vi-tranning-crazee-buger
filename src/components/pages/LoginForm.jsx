27fbf88097713193e53849bd227ed5eebd697061
Arnold Bukasa <arnold.bukasa1@gmail.com>
2024-05-13 15:07:01 +0100

    F02>Loginpage (class to classname

--- src/components/pages/LoginForm.jsx
+++ src/components/pages/LoginForm.jsx
@@ -32,11 +32,11 @@ export default function LoginForm() {
     //affichage
   return (
     <div>
-        <form class="d-flex" action='submit' onSubmit={handleSubmit}>
+        <form className="d-flex" action='submit' onSubmit={handleSubmit}>
         <h1>Bienvenue chez vous</h1>
         <h5>Veuillez vous connecter</h5>
-        <div class="col">
-            <div class="mb-3">
+        <div className="col">
+            <div className="mb-3">
         
                 <input
                     type="text"
@@ -45,12 +45,12 @@ export default function LoginForm() {
                     value={inputPrenom}
                     onChange={(e) =>handleChange(e)}
                     id=""
-                    class="form-control"
+                    className="form-control"
                     placeholder="veuillez enter votre prénom"
                     aria-describedby="helpId"
                 />
                 {/* biome-ignore lint/a11y/useButtonType: <explanation> */}
-                <button  class="form-control" onClick={handleClick}>
+                <button  className="form-control" onClick={handleClick}>
                 Accedez dans votre espace
                 </button>
               
