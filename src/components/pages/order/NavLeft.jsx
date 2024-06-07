37dbeb8cbb0f1b879d4f2c10367a84e7af9ddd8f
Arnold Bukasa <arnold.bukasa1@gmail.com>
2024-06-07 16:46:45 +0100

    :sparkles: OrderPage >Logo>relead

--- src/components/pages/order/NavLeft.jsx
+++ src/components/pages/order/NavLeft.jsx
@@ -3,13 +3,22 @@ import styled from 'styled-components';
 import Logo from '../../reusable-ui/Logo'
 
 export default function NavLeft() {
+
+  //comportement
+  const handleClick = () => { 
+    
+    console.log("onclick cliked")
+    window.location.reload()
+   }
   return (
     <NavLeftstyled className='left-side'>
-        <Logo/>
+        <Logo className="logo-order-page" onClick={()=>handleClick}/>
     </NavLeftstyled>
   )
 }
 const NavLeftstyled = styled.div`
      /* border: 1px solid brown; */
-     
+     .logo-order-page{
+      cursor:pointer;
+     }
 `;
